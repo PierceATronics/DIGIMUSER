42b13b752fa630deb6f58f0dcc7f12eee0ef3744
Danielle <dkstewart@sdsu.edu>
2021-03-17 16:49:24 -0700

    Updated the Header/CPP to follow the new format

--- Tremolo.cpp
+++ Tremolo.cpp
@@ -8,13 +8,48 @@
 
 float tremolo::run(float in, float a)
 {
-	//need to increase each time to increment through the sine wave
-	index = index + 1;
+	
+	
 
-	//effect
-	tremo = (1 + (a * sinhf(40 * PI * index)));
+}
+void tremolo::update(void) 
+{
+	audio_block_t* block;
 
-	//applying effect to the incoming adc data
-	return y = tremo * in;
+	block = receiveWriteable(); //allocate(); is used on Teensy Example
+	
+	if (block) {
+		for (int i = 0; i < AUDIO_BLOCK_SAMPLES; i++) {
+			//creating the effect
+			tremo = (1 + (a * sinhf(40 * PI * i))); // a = intensity, i = index of the sine wave
+			float audio_sample = this->int16_to_float_normalized(block->data[i]);
+			//applying effect to the incoming adc data
+			block->data[i] = this->float_to_int16(tremo * audio_sample);
+		}
+	}
+		
 
+	tremolo::transmit(block);
+	tremolo::release(block);
+
+
+}
+
+
+
+//both of these are stolen from Pierce's Amplify code
+float tremolo::int16_to_float_normalized(int16_t a) {
+
+	float b = (float)a / 32767.0;
+	return(b);
+
+}
+
+//input float must be in range [-1.0, 1.0]
+int16_t tremolo::float_to_int16(float a) {
+
+	if (a > 1.0) a = 1.0;
+	else if (a < -1.0) a = -1.0;
+	int16_t b = (int16_t)(a * 32767.0);
+	return(b);
 }
--- Tremolo.h
+++ Tremolo.h
@@ -1,15 +1,22 @@
+#include "AudioSteam.h"
+
 #ifndef TREMOLO_H
 #define TREMOLO_H
 
-class tremolo
+
+class tremolo : public AudioStream
 {
 
 private:
 	float index;	//time index of the sine wave
 	float tremo;	//the effect 
 	float y;	//the final value after mixing z with the input
+	audio_block_t *inputQueueArray[2]; //the array, x = number of inputs your object will support
 
 public:
+	AudioEffectTemplate() : AudioStream(2, inputQueueArray); //x(2) = number of inputs your object will support
+															//inputQueueArray = an array of audio block pointters used for the inputs
+	virtual void update(void); //the library will call your update function every 128 samples, or about every 2.0 ms
 	float run(float in, float a);	//function used to apply effect
 
 };
